96a5995ef8d896d6336ae40325e4f6b6943de3f5
Khushal212 <96299889+Khushal212@users.noreply.github.com>
2026-04-01 14:42:15 +0530

    Remove unused import for drift package

--- BMA/lib/screens/customers_screen.dart
+++ BMA/lib/screens/customers_screen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:uuid/uuid.dart';
-import 'package:drift/drift.dart' show Value;
 import '../database/app_database.dart';
 import 'customer_detail_screen.dart';
 
