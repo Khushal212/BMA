263d99b140d77eb435dcd1574bef461eba36bcc7
Khushal212 <96299889+Khushal212@users.noreply.github.com>
2026-04-03 11:20:37 +0530

    Update new_invoice_screen.dart

--- BMA/lib/screens/new_invoice_screen.dart
+++ BMA/lib/screens/new_invoice_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:uuid/uuid.dart';
 import '../database/app_database.dart';
+import 'package:drift/drift.dart' show Value;
 
 class NewInvoiceScreen extends StatefulWidget {
   const NewInvoiceScreen({Key? key}) : super(key: key);
